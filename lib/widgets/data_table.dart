c7220b11d5835cc9bfc26282f6bd5cb8787b406b
LeonScot <dmcawahab@gmail.com>
2021-01-17 20:28:01 +0500

    responsive data table

--- lib/widgets/data_table.dart
+++ lib/widgets/data_table.dart
@@ -12,37 +12,88 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   static const int numItems = 20;
   List<bool> selected = List<bool>.generate(numItems, (index) => false);
 
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      child: DataTable(
-        columns: const <DataColumn>[
-          DataColumn(
-            label: Text('Store Name'),
-          ),
-          DataColumn(
-            label: Text('Service Name'),
-          ),
-          DataColumn(
-            label: Text('Date'),
-          ),
-          DataColumn(
-            label: Text('Message'),
-          ),
-        ],
-        rows: List<DataRow>.generate(
-          numItems,
-          (index) => DataRow(
-            cells: [
-              DataCell(Text('Magento $index | D365')),
-              DataCell(Text('Customer Create')),
-              DataCell(Text('18 Jul 2019 18:07:05')),
-              DataCell(Text('Inventory  for ”Phones” successfully synced')),
-            ],
+  Widget _widthAbove900() {
+    return SingleChildScrollView(
+      child: SizedBox(
+        width: double.infinity,
+        child: DataTable(
+          columns: const <DataColumn>[
+            DataColumn(
+              label: Text('Store Name'),
+            ),
+            DataColumn(
+              label: Text('Service Name'),
+            ),
+            DataColumn(
+              label: Text('Date'),
+            ),
+            DataColumn(
+              label: Text('Message'),
+            ),
+          ],
+          rows: List<DataRow>.generate(
+            numItems,
+            (index) => DataRow(
+              cells: [
+                DataCell(Text('Magento $index | D365')),
+                DataCell(Text('Customer Create')),
+                DataCell(Text('18 Jul 2019 18:07:05')),
+                DataCell(Text('Inventory  for ”Phones” successfully synced')),
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  Widget _widthBelow900() {
+    return SizedBox(
+      width: 900,
+      child: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: DataTable(
+            columns: const <DataColumn>[
+              DataColumn(
+                label: Text('Store Name'),
+              ),
+              DataColumn(
+                label: Text('Service Name'),
+              ),
+              DataColumn(
+                label: Text('Date'),
+              ),
+              DataColumn(
+                label: Text('Message'),
+              ),
+            ],
+            rows: List<DataRow>.generate(
+              numItems,
+              (index) => DataRow(
+                cells: [
+                  DataCell(Text('Magento $index | D365')),
+                  DataCell(Text('Customer Create')),
+                  DataCell(Text('18 Jul 2019 18:07:05')),
+                  DataCell(Text('Inventory  for ”Phones” successfully synced')),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      if (constraints.maxWidth >= 900) {
+        return _widthAbove900();
+      } else {
+        return _widthBelow900();
+      }
+    });
   }
 }
